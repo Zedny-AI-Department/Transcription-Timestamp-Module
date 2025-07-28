947ba9616edf514726637f81b78cd9cb93009136
ola172 <olaemadzahran@gmail.com>
2025-07-28 14:02:21 +0300

    feat: Initialize project structure with pyproject.toml and src modules

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+from .api import paragraph_timestamp_router
+
+__all__ = ["paragraph_timestamp_router"]
---
+++ src/api/__init__.py
@@ -0,0 +1,3 @@
+from .paragraph_timestamp_route import paragraph_timestamp_router
+
+__all__ = ["paragraph_timestamp_router"]
